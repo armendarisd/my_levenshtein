8826dcaf1a3caee0a05bb810f4478e352c5dbc21
daniel barrera <barrera_d@qwasar.io>
2021-10-02 03:19:50 +0000

    It is amazing

---
+++ ex00/my_levenshtein.js
@@ -0,0 +1,31 @@
+const str1 = 'hitting';
+const str2 = 'kitte00';
+const levenshteinDistance = (str1 = '', str2 = '') => {
+    if(str1.length==str2.length){
+        const track = Array(str2.length + 1).fill(null).map(() =>
+        Array(str1.length + 1).fill(null));
+   for (let i = 0; i <= str1.length; i += 1) {
+        track[0][i] = i;
+   }
+   for (let j = 0; j <= str2.length; j += 1) {
+        track[j][0] = j;
+   }
+   for (let j = 1; j <= str2.length; j += 1) {
+      for (let i = 1; i <= str1.length; i += 1) {
+            const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
+            track[j][i] = Math.min(
+            track[j][i - 1] + 1, // deletion
+            track[j - 1][i] + 1, // insertion
+            track[j - 1][i - 1] + indicator, // substitution
+         );
+      }
+   }
+            return track[str2.length][str1.length];
+    }
+    else{
+        return -1;
+    }
+
+   
+};
+console.log(levenshteinDistance(str1, str2));
